e9e0ff6cc346bad164af4b5ad6c0793aa3551cc5
anil <av701588@gmail.com>
2025-10-23 20:28:39 +0530

    solved

---
+++ array/mergeSortedArray.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int midx = m - 1;         // Pointer for last element in nums1's valid part
+        int nidx = n - 1;         // Pointer for last element in nums2
+        int right = m + n - 1;    // Pointer for last position in nums1
+
+        // Merge from the end to avoid overwriting elements in nums1
+        while (nidx >= 0) {
+            if (midx >= 0 && nums1[midx] > nums2[nidx]) {
+                nums1[right] = nums1[midx];
+                midx--;
+            } else {
+                nums1[right] = nums2[nidx];
+                nidx--;
+            }
+            right--;
+        }        
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums1 = {1,2,3,0,0,0};
+    vector<int> nums2 = {2,5,6};
+    int m = 3, n = 3;
+
+    sol.merge(nums1, m, nums2, n);
+
+    cout << "Merged array: ";
+    for (int num : nums1) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
