292c3f3855d850d32d2e0585310e894bb806e589
anil <av701588@gmail.com>
2025-10-25 19:49:36 +0530

    solved

---
+++ matrix/rotate.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int edgeLength = matrix.size();
+
+        // Step 1: Reverse the matrix vertically (top ↔ bottom)
+        int top = 0;
+        int bottom = edgeLength - 1;
+
+        while (top < bottom) {
+            for (int col = 0; col < edgeLength; col++) {
+                int temp = matrix[top][col];
+                matrix[top][col] = matrix[bottom][col];
+                matrix[bottom][col] = temp;
+            }
+            top++;
+            bottom--;
+        }
+
+        // Step 2: Transpose the matrix
+        for (int row = 0; row < edgeLength; row++) {
+            for (int col = row + 1; col < edgeLength; col++) {
+                int temp = matrix[row][col];
+                matrix[row][col] = matrix[col][row];
+                matrix[col][row] = temp;
+            }
+        }        
+    }
+};
+
+int main() {
+    Solution sol;
+    int n;
+
+    cout << "Enter size of square matrix (n): ";
+    cin >> n;
+
+    vector<vector<int>> matrix(n, vector<int>(n));
+
+    cout << "Enter matrix elements:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    sol.rotate(matrix);
+
+    cout << "\nMatrix after 90-degree clockwise rotation:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
